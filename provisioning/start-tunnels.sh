00e3a19ee9c52ac3526cdc15e60a9a8155d67551
Albin Stjerna <albin.stjerna@gmail.com>
2018-05-21 18:18:22 +0200

    Use hostnames for everything, ansible setup for hdfs

--- provisioning/start-tunnels.sh
+++ provisioning/start-tunnels.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 USER_NAME=ubuntu
 SERVER_IP=130.238.28.96
+NAME_NODE_IP=namenode
 
-ssh -v \
+ssh \
     -L 8080:localhost:8080 \
     -L 4040:localhost:4040 \
     -L 8888:localhost:8888 \
+    -L 50070:$NAME_NODE_IP:50070 \
     $USER_NAME@$SERVER_IP
